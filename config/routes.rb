382deaebccbc7d4f90d47f39464870a63ff8c830
denniskiprop25@gmail.com <dennis kiprop masiror>
2023-01-29 10:33:46 +0300

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  resources :users
+  # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  # resources :users, only: [:create]
+  resources :users, only: [:create]
   # post '/login', to: 'auth#create'
   # get '/profile', to: 'users#profile'
 
-post "/signup", to: "users#create"
-get '/me', to: 'users#show'
-post '/login', to: 'sessions#create'
-delete '/logout', to: 'sessions#destroy'
+# post "/signup", to: "users#create"
+# get '/me', to: 'users#show'
+# post '/login', to: 'sessions#create'
+# delete '/logout', to: 'sessions#destroy'
 
 end
